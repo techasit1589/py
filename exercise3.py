5ebdd674cf17bf49270689aa1fbc7159bc278e4c
techasit1589 <techasit.sa@ku.th>
2021-01-17 10:34:34 +0700

    งาน

---
+++ exercise3.py
@@ -0,0 +1,32 @@
+# จงแสดง "banana"
+fruits1 = ["apple", "banana", "cherry"]
+print(fruits1[1])
+
+# จงแก้ไขข้อมูลจาก "apple" เป็น "kiwi"
+fruits2 = ["apple", "banana", "cherry"]
+fruits2[0] = "kiwi"
+print(fruits2)
+
+# จงเพิ่ม "kiwi" ไปยัง fruits list
+fruits3 = ["apple", "banana", "cherry"]
+fruits3.append("kiwi")
+print(fruits3)
+
+# จงเพิ่ม "lemon" ไประหว่าง "apple" กับ "ิิbananna"
+fruits4 = ["apple", "banana", "cherry"]
+fruits4.append("lemon"[1])
+print(fruits4)
+
+# จงลบ "cherry" จาก list
+fruits5 = ["apple", "banana", "cherry"]
+fruits5.remove("cherry")
+print(fruits5)
+
+
+# จงแสดงตัวสุดท้ายของ fruits
+fruits6 = ["apple", "banana", "cherry"]
+print(fruits6[2])
+
+# จงแสดงจำนวนของ fruits
+fruits8 = ["apple", "banana", "cherry"]
+print(len(fruits8))
